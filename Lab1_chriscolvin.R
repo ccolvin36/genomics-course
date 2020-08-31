bd6961808fd7315aa3cf034368c37de5f41fc8f7
Chris Colvin <ccolvin@umass.edu>
2020-08-31 10:34:23 -0400

    Upload of previous lab 1 projects, as well as index html

---
+++ Lab1_chriscolvin.R
@@ -0,0 +1,24 @@
+###Exercise 1
+x<-3
+y<-15
+x+y
+x-y
+x*y
+x/y
+
+###Exercise 2
+
+x<-2
+y<-3
+x^5 > y^4
+
+###Exercise 3
+
+vector1 <- c(22,62,148,43,129)
+sum1<- sum (vector1)
+vector1/sum1
+
+###Exercise 4
+
+nucleotides<- c("A", "T", "C", "G")
+sort(nucleotides)
